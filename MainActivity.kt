a3476424d698a5a324584566eca8aa518f4f7370
Bekzat-Chauvbayev <kazna51k@gmail.com>
2023-10-16 12:03:56 +0600

    composearticle

--- MainActivity.kt
+++ MainActivity.kt
@@ -4,96 +4,72 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.bita.myapplication.ui.theme.HappyBirthdayTheme
+import com.bita.myapplication.ui.theme.ComposeArticleTheme
 
-
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HappyBirthdayTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+            ComposeArticleTheme{
+                Surface (
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingImage(
-                        stringResource(R.string.happy_birthday_text),
-                        stringResource(R.string.signature_text)
-                    )
+                ){
+                    ComposeArticleApp()
                 }
+
             }
         }
     }
-}
-@Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    // Create a column so that texts don't overlap
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(top = 16.dp)
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .padding(end = 16.dp)
-                .align(alignment = Alignment.End)
-
+    @Composable
+    fun ComposeArticleApp() {
+        ArticleCard(
+            title = stringResource(R.string.app_name),
+            shortDescription = stringResource(R.string.desc),
+            longDesc = stringResource(R.string.longD),
+            imagePainter = painterResource(R.drawable.bg_compose_background)
         )
     }
-}
 
-@Composable
-fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
-    // Create a box to overlap image and texts
-    Box(modifier) {
-        Image(
-            painter = painterResource(id = R.drawable.androidparty),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
-        )
-        GreetingText(
-            message = message,
-            from = from,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-        )
+    @Composable
+    private fun ArticleCard(
+        title:String,
+        shortDescription: String,
+        longDesc: String,
+        imagePainter: Painter,
+        modifier: Modifier = Modifier,
+
+    ){
+        Column(modifier = modifier){
+            Image(painter = imagePainter, contentDescription = null)
+            Text(text = title, modifier = Modifier.padding(16.dp),
+                fontSize = 24.sp)
+            Text(text = shortDescription, modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                textAlign = TextAlign.Justify)
+            Text(text = longDesc, modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                textAlign = TextAlign.Justify)
+        }
     }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HappyBirthdayTheme {
-        GreetingImage(stringResource(R.string.happy_birthday_text),
-            stringResource(R.string.signature_text))
+    @Preview(showBackground = true)
+    @Composable
+    fun ComposeArticleAppPreview() {
+        ComposeArticleTheme {
+            ComposeArticleApp()
+        }
     }
 }
